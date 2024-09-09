eef2137529aee6a2d4389368ed0d07c637c953b1
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-10 00:43:44 +0700

    Time: 44 ms (97.9%), Space: 11.8 MB (76.73%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = nums*2
+        return ans
+        
