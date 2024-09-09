176b959e6ea2ce8b58841083cff61072b673c97d
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-10 00:40:14 +0700

    Time: 56 ms (57.2%), Space: 11.6 MB (98.83%) - LeetHub

---
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sumIndicesWithKSetBits(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(nums)):
+            if bin(i).count('1') == k:
+                res += nums[i]
+        return res
+        
