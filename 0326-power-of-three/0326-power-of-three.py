383f61248b607faa4524cebe041e43b65c95eca9
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-10 00:44:05 +0700

    Time: 46 ms (86.03%), Space: 11.5 MB (71.88%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        if n % 3 != 0:
+            return False
+        return self.isPowerOfThree(n/3)
