a24247d380a36c32de3e6b7785807b08c44f2acd
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-10 00:41:06 +0700

    Time: 19 ms (36.68%), Space: 11.4 MB (99.5%) - LeetHub

---
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for i, j in zip(nums,index):
+            res.insert(j,i)
+        return res
+
