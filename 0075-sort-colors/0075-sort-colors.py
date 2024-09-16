57bad0eeee718798a75be6ab108fd9ca7b861cd7
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-16 14:31:24 +0700

    Time: 39 ms (46.63%), Space: 16.6 MB (23.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,7 +1,29 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        def partition(nums, left, right):
+            pIndex = left
+            key = nums[right]
+            i = left
+            while i < right:
+                if nums[i] < key:
+                    temp = nums[i]
+                    nums[i] = nums[pIndex]
+                    nums[pIndex] = temp
+                    pIndex += 1
+                i += 1
+            temp = nums[pIndex]
+            nums[pIndex] = nums[right]
+            nums[right] = temp
+            return pIndex
+        def quick_sort(nums, left, right):
+            if left < right:
+                p = partition(nums, left, right)
+                quick_sort(nums, left, p - 1)
+                quick_sort(nums, p + 1, right)
+            return nums
+        
+        res = quick_sort(nums, 0, len(nums) - 1)
+        return res
