a047f7945f7e9385eec61ae15a92d200fcedfac9
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-19 12:57:14 +0700

    Time: 16 ms (36.74%), Space: 11.7 MB (80.05%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.py
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.py
@@ -5,19 +5,12 @@ class Solution(object):
         :rtype: str
         """
         s = s.split(" ")
-        res = ""
-        map_s = []
-        for i in range(len(s)):
-            map_s.append((s[i][-1],s[i][:-1]))
-
-        print(map_s)
-        map_s.sort()
-        for i in range(len(map_s)):
-            if i == 0:
-                res += map_s[i][1]
-            else:
-                res += " "
-                res += map_s[i][1]
-
-
-        return res
+        hashmap = []
+        for item in s:
+            hashmap.append((item[-1], item[:-1]))
+        hashmap.sort()
+        s = ""
+        for i in range(len(hashmap)):
+            s += (" " if i != 0 else "") + hashmap[i][1]
+        return s
+            
