015afce71b6c03c51d9dc62eaab2aff165bc196e
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-19 12:33:02 +0700

    Time: 327 ms (46.98%), Space: 14.6 MB (7.12%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
@@ -5,12 +5,12 @@ class Solution(object):
         :type words: List[str]
         :rtype: int
         """
-        minus_res = 0
-        for i in words:
-            for j in range(len(i)):
-                if i[j] not in allowed:
-                    minus_res += 1
+        count = len(words)
+        for i in range(0, len(words)):
+            key = words[i]
+            for j in range(0, len(key)):
+                if key[j] not in allowed:
+                    count -= 1
                     break
-        return len(words) - minus_res
-
-        
+        return count
+                
