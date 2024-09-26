1c6c20328157fed71e03bfc979a484a6b392d20b
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-26 14:58:29 +0700

    Time: 38 ms (94.95%), Space: 16.6 MB (29.79%) - LeetHub

--- 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.py
+++ 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.py
@@ -1,18 +1,4 @@
-class Solution(object):
-    def minOperations(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        count = 0
-        n = len(nums)
-        for i in range(n):
-            min_a = min(nums)
-            if min_a >= k:
-                return count
-            else:
-                count += 1
-                nums.remove(min_a)
-        return count
-        
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return len(list(filter(lambda x: x < k, nums)))
