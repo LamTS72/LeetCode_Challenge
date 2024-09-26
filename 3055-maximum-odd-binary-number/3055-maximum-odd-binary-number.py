e5b4af7af544a08f7dee738f85bbe75252e13959
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-26 14:50:02 +0700

    Time: 40 ms (45.62%), Space: 16.6 MB (39.99%) - LeetHub

--- 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
@@ -1,12 +1,5 @@
-class Solution(object):
-    def maximumOddBinaryNumber(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
         if s.count('1') == 1:
-            return s.count('0')*'0' + '1'
-
-        return (s.count('1')-1)*'1' + s.count('0')*'0' + '1'
-
-        
+            return ('0' * s.count('0')) + '1'
+        return '1' * (s.count('1') - 1) + ('0' * s.count('0')) + '1'
