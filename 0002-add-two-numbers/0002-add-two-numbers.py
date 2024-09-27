c0e4e2af23c81cddbb8922d461d057bf4acfacaf
Son Lam Trinh <sonlam7220@gmail.com>
2024-09-27 13:59:11 +0700

    Time: 66 ms (5.46%), Space: 16.6 MB (76.56%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,44 +1,32 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        currA = l1
-        listA = []
-        while currA is not None:
-            listA.append(currA.val)
-            currA = currA.next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if not l1 and not l2:
+            return None
         
-        currB = l2
-        listB = []
-        while currB is not None:
-            listB.append(currB.val)
-            currB = currB.next
+        s1 = ""
+        s2 = ""
+        while l1:
+            s1 += str(l1.val)
+            l1 = l1.next
+        while l2:
+            s2 += str(l2.val)
+            l2 = l2.next
         
-        print(listA, listB)
-        stringA = ""
-        for i in range(len(listA)):
-            stringA += str(listA.pop())
-        
-        stringB = ""
-        for i in range(len(listB)):
-            stringB += str(listB.pop())
-        
-        stringC = int(stringA) + int(stringB)
-        stringC = str(stringC)[::-1]
-        print(stringC)
-        dummy = curr = ListNode(0)
-        for i in stringC:
-            curr.next = ListNode(int(i))
+        total = int(s1[::-1]) + int(s2[::-1])
+        print(total)
+        dumpy = curr = ListNode()
+        for s in str(total)[::-1]:
+            print(s, type(s))
+            curr.next = ListNode(int(s))
             curr = curr.next
+        return dumpy.next
 
-        return dummy.next
-        
-        
