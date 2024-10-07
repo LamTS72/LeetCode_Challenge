b9960a0e9a9df28c2b1266593b38c9898cb77801
Son Lam Trinh <sonlam7220@gmail.com>
2024-10-08 05:04:05 +0700

    Time: 470 ms (88.26%), Space: 14 MB (95.57%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -4,23 +4,21 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: List[List[int]]
         """
-        q = []
+        queue = []
         row = len(mat)
         col = len(mat[0])
-        for i in range(0, row):
-            for j in range(0, col):
-                if mat[i][j] == 0:
-                    q.append((i, j))
+        for r in range(row):
+            for c in range(col):
+                if mat[r][c] == 0:
+                    queue.append((r, c))
                 else:
-                    mat[i][j] = '#'
-
-        for i, j in q:
-            for dx, dy in (-1,0), (1, 0), (0, 1), (0, -1):
-                nr = i + dx
-                nc = j + dy
-                if 0 <= nr < row and 0<= nc < col and mat[nr][nc] == '#':
-                    mat[nr][nc] = mat[i][j] + 1
-                    q.append((nr, nc))
+                    mat[r][c] = "#"        
+        
+        for i,j in queue:
+            for di, dj in (0, 1), (1, 0), (0, -1), (-1, 0):
+                r = i + di
+                c = j + dj
+                if 0 <= r < row and 0 <= c < col and mat[r][c] == "#":
+                    mat[r][c] = mat[i][j] + 1
+                    queue.append((r, c))
         return mat
-        
-        
