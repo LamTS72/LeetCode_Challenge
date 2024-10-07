4e36896b914150e2977f98d76aa03b33a4a1aae6
Son Lam Trinh <sonlam7220@gmail.com>
2024-10-08 05:31:13 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -1,27 +1,18 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def pairSum(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: int
-        """
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
         list_a = []
         list_b = []
         curr = head
-        while curr is not None:
+        while curr:
             list_a.append(curr.val)
             curr = curr.next
-        
-        for i in range(len(list_a)/2):
-            tmp = list_a[i] + list_a[len(list_a)-1-i]
-            list_b.append(tmp)
-        res = max(list_b)
-        print(res)
-        return res
-
+        for i in range(0, (len(list_a)//2) - 1):
+            sum_p = list_a[i] + list_a[len(list_a) - i - 1]
+            list_b.append(sum_p)
             
-        
+        return max(list_b)
