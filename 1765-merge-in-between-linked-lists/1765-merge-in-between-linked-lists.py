783b3b5dc599d86ea60f499a70b370336011d1ac
Son Lam Trinh <sonlam7220@gmail.com>
2024-10-08 05:23:27 +0700

    Time: 351 ms (11.71%), Space: 22.6 MB (94.63%) - LeetHub

--- 1765-merge-in-between-linked-lists/1765-merge-in-between-linked-lists.py
+++ 1765-merge-in-between-linked-lists/1765-merge-in-between-linked-lists.py
@@ -12,25 +12,22 @@ class Solution(object):
         :type list2: ListNode
         :rtype: ListNode
         """
-        idx = 0
-        curr1 = list1
+        i = 0
         tail_a = None
         tail_b = None
-        while curr1 is not None:
-            if idx == a - 1:
-                tail_a = curr1
-            if idx == b + 1:
-                tail_b = curr1
-
-            curr1 = curr1.next
-            idx += 1
-        #print(tail_a.val,tail_b.val)
+        curr = list1
+        while curr:
+            if i == a - 1:
+                tail_a = curr
+            if i == b + 1:
+                tail_b = curr
+            curr = curr.next
+            i += 1
+        
         tail_a.next = list2
         curr2 = list2
-        while curr2 is not None and curr2.next is not None:
+        while curr2.next is not None:
             curr2 = curr2.next
         curr2.next = tail_b
-
         return list1
-
-
+        
