458a15ea53e9eccbb5fd2e9984bdb741d28d2431
Son Lam Trinh <sonlam7220@gmail.com>
2024-10-07 23:48:47 +0700

    Time: 48 ms (84.65%), Space: 15.1 MB (58.86%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -5,23 +5,22 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
+        i = 0
         i0 = newInterval[0]
         i1 = newInterval[1]
-        i = 0
         res = []
-        #first case
-        while i < len(intervals) and intervals[i][1] < i0:
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
             res.append(intervals[i])
-            i = i + 1
-        #overlap case
-        while i < len(intervals) and intervals[i][0] <= i1:
-            i0 = min(i0, intervals[i][0])
-            i1 = max(i1, intervals[i][1])
-            i = i + 1
-        res.append([i0,i1])
-        #remains
+            i += 1
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            print(intervals[i][0],newInterval[0])
+            i0 = min(intervals[i][0], i0)
+            i1 = max(intervals[i][1], i1)
+            i += 1
+        res.append([i0, i1])
         while i < len(intervals):
             res.append(intervals[i])
-            i = i + 1
+            i += 1
+
         return res
         
