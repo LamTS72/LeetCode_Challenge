0c67834e60c076b31706a4047756da86c9d364ba
Son Lam Trinh <sonlam7220@gmail.com>
2024-12-05 00:49:31 +0700

    Time: 1504 ms (10.1%), Space: 19.6 MB (87.71%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,35 +1,20 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        nums.sort(reverse=False)
-        print(nums)
-        n = len(nums) - 2
-        i = 0
-        res = []
-        while i < n:
-            if i >= 1 and nums[i] == nums[i-1]:
-                i += 1
-                continue
-            left = i + 1
-            right = len(nums) - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total > 0:
-                    right -= 1
-                elif total < 0:
-                    left += 1
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Solution: loop
+        # Requirement: O(n^2)
+        nums = sorted(nums)
+        res = set()
+        for i in range(len(nums)):
+            ileft = i + 1
+            iright = len(nums) - 1
+            while ileft < iright:
+                isum = nums[i] + nums[ileft] + nums[iright]
+                if isum < 0:
+                    ileft += 1
+                elif isum > 0:
+                    iright -= 1
                 else:
-                    triple0, triple1, triple2 = nums[i], nums[left], nums[right]
-                    res.append([triple0, triple1, triple2])
-                    while left < right and nums[left] == triple1:
-                        left += 1
-                    while left < right and nums[right] == triple2:
-                        right -= 1
-            i += 1
-        print(res)
-
-        return res
-
+                    res.add((nums[i], nums[ileft], nums[iright]))
+                    ileft += 1
+                    iright -= 1
+        return list(res)
