18e622c3ffd925180d041791b704eacbb2a2e7de
Son Lam Trinh <sonlam7220@gmail.com>
2024-12-04 23:38:49 +0700

    Time: 11 ms (52.23%), Space: 34.4 MB (8.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,11 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # Solution: one loop
+        # Requirement: O(n)
         hashmap = {}
         for num in nums:
             if num in hashmap:
-                hashmap[num] += 1
-                if hashmap[num] > 1:
-                    return True
+                return True
             else:
                 hashmap[num] = 1
         return False
