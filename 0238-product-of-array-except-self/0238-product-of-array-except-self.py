5776fd174e031e4e2fb2e610c50cc6cc6cc7e031
Son Lam Trinh <sonlam7220@gmail.com>
2024-12-05 16:58:17 +0700

    Time: 25 ms (52.91%), Space: 22.8 MB (59.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,24 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        if len(nums) <= 1:
-            return sum(num)
-        product = 1
+        # Solution: loop
+        # Requirement: O(n)
+        iproduct = 1
         for num in nums:
             if num != 0:
-                product *= num
-            
-        count0 = nums.count(0)
-        res = []
-        for i,num in enumerate(nums):
-            if num == 0:
-                res = [0]*len(nums)
-                if count0 > 1:
+                iproduct *= num
+        res = len(nums)*[None]
+        count_zero = nums.count(0)
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                res = len(nums)*[0]
+                if count_zero > 1:
                     return res
                 else:
-                    res[i] = product
+                    res[i] = iproduct
                     return res
             else:
-                res.append(int(product / num))
+                res[i] = int(iproduct/nums[i])
         return res
 
         
