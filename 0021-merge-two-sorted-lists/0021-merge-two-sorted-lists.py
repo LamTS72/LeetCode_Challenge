2ff44ded3e4a317a179516824449bfddb7604d77
Son Lam Trinh <sonlam7220@gmail.com>
2024-12-10 22:47:49 +0700

    Time: 0 ms (100%), Space: 17.5 MB (7.14%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,34 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # Solution: loop
+        # Requirement: O(n)
         if list1 is None:
             return list2
         if list2 is None:
             return list1
-        dumpy = ListNode()
-        curr = dumpy
+        
+        dummyNode = curr = ListNode()
         while list1 is not None and list2 is not None:
             if list1.val < list2.val:
-                curr.next = list1
+                curr.next = ListNode(val=list1.val)
                 list1 = list1.next
             else:
-                curr.next = list2
+                curr.next = ListNode(val=list2.val)
                 list2 = list2.next
             curr = curr.next
-        if list1 is not None:
-            curr.next = list1
         
-        if list2 is not None:
+        if list1 is None:
             curr.next = list2
-
-        return dumpy.next
-
+        if list2 is None:
+            curr.next = list1
+        return dummyNode.next
