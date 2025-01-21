ccc361316eb95ee88a32e5420b852032ad4537d6
Son Lam Trinh <sonlam7220@gmail.com>
2025-01-21 18:18:08 +0700

    Time: 2 ms (55.86%), Space: 19.1 MB (7.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Solution: 2 pointers
-        # Requirement: less O(n^2)
         hashmap = dict()
-        for i, v in enumerate(nums): # O(n)
-            hashmap[v] = i
-        for i, v in enumerate(nums): # O(n)
-            part = target - v
-            if part in hashmap and hashmap[part] != i:
-                return i, hashmap[part]
-                
+        for i, num in enumerate(nums):
+            hashmap[num] = i
+        
+        for i, num in enumerate(nums):
+            remain = target - num
+            if remain in hashmap and hashmap[remain] != i:
+                return i, hashmap[remain]
