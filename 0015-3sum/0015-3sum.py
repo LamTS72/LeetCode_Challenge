c36af63f8bfe9f4745b40b7823e64dbf6b86d052
Son Lam Trinh <sonlam7220@gmail.com>
2025-01-22 17:29:58 +0700

    Time: 1037 ms (18.57%), Space: 21 MB (16.38%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,20 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # Solution: loop
-        # Requirement: O(n^2)
-        nums = sorted(nums)
         res = set()
+        nums = sorted(nums)
         for i in range(len(nums)):
-            ileft = i + 1
-            iright = len(nums) - 1
-            while ileft < iright:
-                isum = nums[i] + nums[ileft] + nums[iright]
-                if isum < 0:
-                    ileft += 1
-                elif isum > 0:
-                    iright -= 1
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                sumt = nums[i] + nums[left] + nums[right]
+                if sumt > 0:
+                    right -= 1
+                elif sumt < 0:
+                    left += 1
                 else:
-                    res.add((nums[i], nums[ileft], nums[iright]))
-                    ileft += 1
-                    iright -= 1
+                    t1, t2, t3 = nums[i], nums[left], nums[right]
+                    res.add((t1, t2, t3))
+                    while left < right and nums[left] == t2:
+                        left += 1
+                    while left < right and nums[right] == t3:
+                        right -= 1
+        print(res)
         return list(res)
+ 
+
+        
