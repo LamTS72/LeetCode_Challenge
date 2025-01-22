f968e216968b17f73fc766e3c306f4b07ad66c37
Son Lam Trinh <sonlam7220@gmail.com>
2025-01-22 17:09:44 +0700

    Time: 4 ms (21.19%), Space: 19.6 MB (42.17%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,26 +1,21 @@
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        """
-        :type intervals: List[List[int]]
-        :type newInterval: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         i = 0
-        i0 = newInterval[0]
-        i1 = newInterval[1]
+        nleft = newInterval[0]
+        nright = newInterval[1]
+        n = len(intervals)
         res = []
-        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+        while i < n and intervals[i][1] < nleft:
             res.append(intervals[i])
             i += 1
-        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
-            print(intervals[i][0],newInterval[0])
-            i0 = min(intervals[i][0], i0)
-            i1 = max(intervals[i][1], i1)
+        while i < n and intervals[i][0] <= nright:
+            nleft = min(intervals[i][0], nleft)
+            nright = max(intervals[i][1], nright)
             i += 1
-        res.append([i0, i1])
-        while i < len(intervals):
+        res.append([nleft, nright])
+        while i < n:
             res.append(intervals[i])
             i += 1
 
         return res
-        
+            
