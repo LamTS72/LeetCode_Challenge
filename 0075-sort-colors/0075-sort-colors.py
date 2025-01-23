6f26cb076db5b91c9f7c005b5c88d154113dd74d
Son Lam Trinh <sonlam7220@gmail.com>
2025-01-23 14:48:36 +0700

    Time: 0 ms (100%), Space: 18 MB (7.86%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,27 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def partition(nums, left, right):
-            key = nums[right]
-            pIndex = left
-            i = left
-            while i < right:
-                if nums[i] < key:
-                    temp = nums[i]
-                    nums[i] = nums[pIndex]
-                    nums[pIndex] = temp
-                    pIndex += 1
-                i += 1
-            temp = nums[pIndex]
-            nums[pIndex] = nums[right]
-            nums[right] = temp
-            return pIndex
- 
         def quicksort(nums, left, right):
             if left < right:
                 p = partition(nums, left, right)
                 quicksort(nums, left, p - 1)
                 quicksort(nums, p + 1, right)
 
+        def partition(nums, left, right):
+            key = nums[right]
+            pIndex = left
+            i = left
+            while i < right:
+                if nums[i] < key:
+                    nums[i], nums[pIndex] = nums[pIndex], nums[i]
+                    pIndex += 1
+                i += 1
+            nums[right], nums[pIndex] = nums[pIndex], nums[right]
+            return pIndex
+
         quicksort(nums, 0, len(nums) - 1)
         return nums
+                
