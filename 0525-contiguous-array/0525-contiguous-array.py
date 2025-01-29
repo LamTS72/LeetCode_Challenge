033c020e660dfc9bc662c9388ae8bc9979c5ab51
Son Lam Trinh <sonlam7220@gmail.com>
2025-01-30 02:26:43 +0700

    Time: 96 ms (84.31%), Space: 23.6 MB (20.26%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -8,9 +8,10 @@ class Solution:
                 count -= 1
             else:
                 count += 1
-
+            
             if count in hashmap:
                 max_len = max(max_len, i - hashmap[count])
             else:
                 hashmap[count] = i
+
         return max_len
