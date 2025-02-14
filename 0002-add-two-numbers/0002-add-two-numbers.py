24c654885b9aab3bec601356b07b15baa08a35c0
Son Lam Trinh <sonlam7220@gmail.com>
2025-02-14 14:26:17 +0700

    Time: 5 ms (43.26%), Space: 17.7 MB (82.61%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,28 +5,39 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        if not l1 and not l2:
-            return None
-        
-        s1 = ""
-        s2 = ""
-        while l1:
-            s1 += str(l1.val)
-            l1 = l1.next
-        while l2:
-            s2 += str(l2.val)
-            l2 = l2.next
-        
-        total = int(s1[::-1]) + int(s2[::-1])
-        print(total)
-        dumpy = curr = ListNode()
-        for s in str(total)[::-1]:
-            print(s, type(s))
-            curr.next = ListNode(int(s))
-            curr = curr.next
-        return dumpy.next
+        l1 = self.reverse(l1)
+        l2 = self.reverse(l2)
 
+        n1 = self.convert(l1)
+        n2 = self.convert(l2)
+
+        n = n1 + n2
+        return self.split(n)
+
+
+    def reverse(self, node):
+        curr = node
+        prev = None
+        while curr:
+            next_curr = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_curr
+        return prev
+
+    def convert(self, node):
+        curr = node
+        num = 0
+        while curr:
+            num = num * 10 + curr.val
+            curr = curr.next
+        return num
+
+    def split(self, n):
+        s = str(n)
+        dumpyNode = ListNode()
+        curr = dumpyNode
+        for i in range(len(s) - 1, -1, -1):
+            curr.next = ListNode(int(s[i]))
+            curr = curr.next
+        return dumpyNode.next
