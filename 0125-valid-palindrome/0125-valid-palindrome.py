57fd2f246ce8498a46984bb2495d4ff20d274ec1
Son Lam Trinh <sonlam7220@gmail.com>
2025-02-17 04:35:44 +0700

    Time: 16 ms (11.19%), Space: 18.2 MB (47.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,18 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # Solution: loop
-        # Requirement: O(n)
-        if len(s) == 0 or s == " " or len(s) == 1:
+        if len(s) == 1 or s == " ":
             return True
-        letters = "qwertyuiopasdfghjklzxcvbnm1234567890"
-        letters = set(letters)
-
+        
         res = ""
         for i in range(len(s)):
-            if s[i].lower() in letters:
+            if "a" <= s[i].lower() <= "z" or "0" <= s[i] <= "9":
                 res += s[i].lower()
         print(res)
-        for i in range(len(res)):
-            if res[i] != res[len(res) - 1 - i]:
-                return False
-        return True
+        return res == res[::-1]
