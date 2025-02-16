b1462fee24e0d1e3d5b0a43c082f815bcd53a21f
Son Lam Trinh <sonlam7220@gmail.com>
2025-02-17 04:46:01 +0700

    Time: 0 ms (100%), Space: 17.8 MB (46.51%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,21 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        # Solution: loop
-        # Requirement: O(n)
-        if len(s) == 1:
-            return 1
         hashmap = dict()
-        for i in range(len(s)):
-            if s[i] in hashmap:
-                hashmap[s[i]] += 1
+        for st in s:
+            if st in hashmap:
+                hashmap[st] += 1
             else:
-                hashmap[s[i]] = 1
+                hashmap[st] = 1
+        
+        odd = False
         length = 0
-        sub = False
-        for k, freq in hashmap.items():
+        for key, freq in hashmap.items():
             if freq % 2 == 0:
                 length += freq
             else:
                 length += freq - 1
-                sub = True
-        return length + 1 if sub == True else length
+                odd = True
+        return length + 1 if odd else length
+        
