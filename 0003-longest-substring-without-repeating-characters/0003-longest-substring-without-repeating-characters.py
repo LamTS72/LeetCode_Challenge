8084d4fc929254636ea2432d2ff6e07ead02d191
Son Lam Trinh <sonlam7220@gmail.com>
2025-02-21 00:28:30 +0700

    Time: 3186 ms (6.09%), Space: 14.4 MB (5.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -6,20 +6,19 @@ class Solution(object):
         """
         if len(s) == 1:
             return 1
-    
-        res = 0
-        for i in range(0,len(s) - 1):
-            sub_s = s[i]
-            max_len = 1
-            for j in range(i+1, len(s)):
-                if s[j] not in sub_s:
-                    sub_s += s[j]
-                    max_len += 1
-                    
+        
+        max_length = 0
+        for i in range(len(s) - 1):
+            tmp_s = s[i]
+            length = 1
+            for j in range(i + 1, len(s)):
+                if s[j] not in tmp_s:
+                    tmp_s += s[j]
+                    length += 1
                 else:
                     break
-            res = max(res, max_len)
-        return res
+            max_length = max(max_length, length)
+        return max_length
 
 
         
