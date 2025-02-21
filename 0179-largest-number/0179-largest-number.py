07eeff1546ed9e1dd160ab2b8a996e6293dd9618
Son Lam Trinh <sonlam7220@gmail.com>
2025-02-21 15:42:14 +0700

    Time: 31 ms (5.05%), Space: 18 MB (15.94%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,37 +1,10 @@
-# class Solution:
-#     def largestNumber(self, nums: List[int]) -> str:
-#         nums = [str(x) for x in nums]
-#         nums.sort(reverse=True, key=lambda x: x * 10) 
-#         isUsed = [False] * len(nums)
-#         max_num = 0
-#         def backtracking(num):
-#             nonlocal max_num
-#             if len(num) == len(nums):
-#                 max_num = max(int("".join(num)), max_num)
-#                 return 
-            
-#             for j in range(len(nums)):
-#                 if isUsed[j] == False:
-#                     num.append(nums[j])
-#                     isUsed[j] = True
-#                     backtracking(num)
-#                     num.pop()
-#                     isUsed[j] = False
-
-                
-#         if len(nums) == 1:
-#             return str(nums[0])
-
-#         num = []
-#         backtracking(num)
-#         print(max_num)
-        
-#         return str(max_num)
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         for i in range(len(nums)):
             for j in range(0,len(nums)-1-i):
                 if str(nums[j]) + str(nums[j+1]) < str(nums[j+1]) + str(nums[j]):
                     nums[j], nums[j+1] = nums[j+1], nums[j]
+
+        print(nums)
         result = list(map(str, nums))
         return ''.join(result) if nums[0] != 0 else "0"
