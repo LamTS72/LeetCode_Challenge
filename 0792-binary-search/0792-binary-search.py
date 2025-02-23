b7ab182b1b41e986cb448989f0577a80b4189751
Son Lam Trinh <sonlam7220@gmail.com>
2025-02-24 02:16:52 +0700

    Time: 3 ms (8.18%), Space: 19 MB (6.62%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,27 +1,29 @@
 class Solution:
-    def iterator_bsearch(self, nums, target):
+    def iterator(self, nums, target):
         left = 0
         right = len(nums) - 1
-
         while left <= right:
-            mid = (right + left) // 2
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
+            elif nums[mid] > target:
                 right = mid - 1
+            else:
+                left = mid + 1 
         return -1
-    def recursive_bsearch(self, nums, target,left, right):
+
+    def recursive(self, nums, target, left, right):
         if left > right:
             return -1
+        
         mid = (left + right) // 2
         if nums[mid] == target:
             return mid
-        elif nums[mid] < target:
-            return self.recursive_bsearch(nums, target, left + 1, right)
+        elif nums[mid] > target:
+            return self.recursive(nums, target, left, mid - 1)
         else:
-            return self.recursive_bsearch(nums, target, left, right - 1)
+            return self.recursive(nums, target, mid + 1, right)
+
     def search(self, nums: List[int], target: int) -> int:
-        print("Iterator Result: ", self.iterator_bsearch(nums, target))
-        return self.recursive_bsearch(nums, target, 0, len(nums) - 1)
+        print(self.iterator(nums, target))
+        return self.recursive(nums, target, 0, len(nums) - 1)
